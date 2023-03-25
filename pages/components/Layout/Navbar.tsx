02fa6678a83b39951388d6c27996b0bb627383c2
NITIN KUMAR <74142111+Nitin443@users.noreply.github.com>
2023-03-25 11:32:18 +0000

    add humberger icon

--- pages/components/Layout/Navbar.tsx
+++ pages/components/Layout/Navbar.tsx
@@ -1,38 +1,43 @@
-import Link from 'next/link';
-import Image from 'next/image';
+import { MenuOutlined } from "@ant-design/icons";
+import { Button, Image } from "antd";
+import Link from "next/link";
+import { useState } from "react";
 import styles from "../../../styles/Navbar.module.scss";
 
 const Navbar = () => {
+
+  const [show, setShow] = useState(false);
+
+  const handleClick = () => {
+    setShow(!show);
+  }
+
   return (
     <nav className={styles.navbar}>
-      <div className={styles.container}>
-        <Link className={styles.navbarLogo} href="/">
-            <Image src="/logo.svg" alt="Logo" width={80} height={80} />
-            My Website
-        </Link>
-        <ul className={styles.navbarLinks}>
-          <li className={styles.navbarItem}>
-            <Link className={styles.navbarLink} href="/">
-              Home
-            </Link>
-          </li>
-          <li className={styles.navbarItem}>
-            <Link className={styles.navbarLink} href="/about">
-              About
-            </Link>
-          </li>
-          <li className={styles.navbarItem}>
-            <Link className={styles.navbarLink} href="/contact">
-              Contact Us
-            </Link>
-          </li>
-          <li className={styles.navbarItem}>
-            <Link className={`${styles.navbarLink} ${styles.button} ${styles.navbarLink}`} href="/login">
-              Login/Signup
-            </Link>
-          </li>
-        </ul>
+
+      <div className={styles.logoDiv} >
+        <Image height={60} width={60} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQy8ivjh3jEgWs6uv7DMzADcxPcFaWP00H1hQ&usqp=CAU" />
       </div>
+
+     
+
+      <div className={styles.linkDiv}  >
+
+       <Link className={styles.link} href="#" > Home  </Link>
+
+       <Link className={styles.link} href="#" > Events  </Link>
+
+       <Link className={styles.link} href="#" > Connects  </Link>
+
+       <Link className={styles.link} href="#" > Roadmaps  </Link>
+
+      </div>
+      <div className={styles.btnDiv} >
+        <Button>Sign in/Log in</Button>
+      </div>
+
+      <div className={styles.humbergDiv} onClick={handleClick} > <MenuOutlined /> </div>
+      
     </nav>
   );
 };

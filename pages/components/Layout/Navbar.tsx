48545f56ed291d3b590e7e57df0d1a297c59b230
NITIN KUMAR <74142111+Nitin443@users.noreply.github.com>
2023-03-28 14:17:35 +0000

    fix error

--- pages/components/Layout/Navbar.tsx
+++ pages/components/Layout/Navbar.tsx
@@ -1,6 +1,5 @@
-import { BorderOuterOutlined, MenuOutlined } from "@ant-design/icons";
-import { Button, Image } from "antd";
-import Link from "next/link";
+import { MenuOutlined, } from "@ant-design/icons";
+import { Image } from "antd";
 import { useState } from "react";
 import styles from "../../../styles/Navbar.module.scss";
 
@@ -8,13 +7,14 @@ const Navbar = () => {
   const [click, setClick] = useState(false);
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
-  
+
   return (
     <div className={styles.header}>
       <div className={styles.logoNav}>
         <div className={styles.logoContainer}>
           <a href="#">
-          <BorderOuterOutlined  className={styles.logo} />
+            <Image preview={false} src="https://tpsowsaa.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fgreen-logo.f704c8aa.png&w=96&q=75" />
+
           </a>
         </div>
 
@@ -28,10 +28,10 @@ const Navbar = () => {
           <li className={styles.option} onClick={closeMobileMenu}>
             <a href="#">BLOG</a>
           </li>
-          <li className={ `${styles.option} ${styles.mobileOption}`} onClick={closeMobileMenu}>
+          <li className={`${styles.option} ${styles.mobileOption}`} onClick={closeMobileMenu}>
             <a href="#">SIGN-IN</a>
           </li>
-          <li className={ `${styles.option} ${styles.mobileOption}`} onClick={closeMobileMenu}>
+          <li className={`${styles.option} ${styles.mobileOption}`} onClick={closeMobileMenu}>
             <a href="" className="sign-up">
               SIGN-UP
             </a>
@@ -49,11 +49,9 @@ const Navbar = () => {
         </li>
       </ul>
       <div className={styles.mobileMenu} onClick={handleClick}>
-        {click ? (
-          <BorderOuterOutlined className={styles.menuIcon} />
-        ) : (
-          <BorderOuterOutlined className={styles.menuIcon} />
-        )}
+
+        <MenuOutlined className={styles.menuIcon} />
+
       </div>
     </div>
   );
